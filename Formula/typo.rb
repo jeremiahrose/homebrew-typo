22f6482826db02b3f97ebba8e7f648b80970dd56
Jeremiah Rose <jeremiahrose88@gmail.com>
2024-10-21 15:56:31 +1100

    Update to `0.2.1`

--- Formula/typo.rb
+++ Formula/typo.rb
@@ -1,7 +1,7 @@
 class Typo < Formula
   desc "AI powered terminal agent"
   homepage "https://github.com/jeremiahrose/typo"
-  url "https://github.com/jeremiahrose/typo.git", tag: "0.2.0"
+  url "https://github.com/jeremiahrose/typo.git", tag: "0.2.1"
   license "MIT"
   head "https://github.com/jeremiahrose/typo.git", branch: "main"
 
